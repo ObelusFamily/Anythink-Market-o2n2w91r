f7c343ea466c8bb37e50b22d02749daa35da374a
Anastasia Malkova <31183167+maliok1@users.noreply.github.com>
2022-12-19 19:14:23 +0000

    fit the whole height

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -32,7 +32,7 @@ const ItemPreview = (props) => {
   return (
     <div
       className="card bg-dark border-light p-3"
-      style={{ borderRadius: "20px", minHeight: "fit-content", height: "100%"}}
+      style={{ borderRadius: "20px"}}
       id={`item_${item.slug}`}
     >
       <img
